7c9b3b92a8263c35d359574b5c3b40f910bd3087
hexa0 <67880600+hexa0@users.noreply.github.com>
2025-12-23 17:22:58 -0500

    [pipewire] implement soundboard output

--- pipewire/init.sh
+++ pipewire/init.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-sleep 5
+sleep 2
 
-PW_LINK="/usr/bin/pw-link"
-PW_JACK="/usr/bin/pw-jack"
-
-"$PW_LINK" sm57-carla:capture_FL alsa_output.usb-Topping_E2x2-00.analog-surround-21:playback_FL
-"$PW_LINK" sm57-carla:capture_FR alsa_output.usb-Topping_E2x2-00.analog-surround-21:playback_FR
+pw-link sm57-carla:capture_FL alsa_output.usb-Topping_E2x2-00.analog-surround-21:playback_FL
+pw-link sm57-carla:capture_FR alsa_output.usb-Topping_E2x2-00.analog-surround-21:playback_FR
+pw-link soundboard_audio_sink:monitor_FL sm57-carla:input_FL
+pw-link soundboard_audio_sink:monitor_FR sm57-carla:input_FR
