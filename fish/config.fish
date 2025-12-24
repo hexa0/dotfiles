04997536aca24ceda89a4abafb0ce804b89c9656
hexa0 <67880600+hexa0@users.noreply.github.com>
2025-12-24 12:51:35 -0500

    fix bun

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-if test -d $HOME/.local/share/.bun/
+if test -d $HOME/.bun/
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
