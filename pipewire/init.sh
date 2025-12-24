87422ced73d21db8749e10a173f5e548349142b6
hexa0 <67880600+hexa0@users.noreply.github.com>
2025-12-23 19:10:45 -0500

    repowering the e2x2 is now handled

--- pipewire/init.sh
+++ pipewire/init.sh
@@ -1,8 +1,40 @@
 #!/bin/bash
 
-sleep 2
+INTERFACE_NODE="alsa_output.usb-Topping_E2x2-00.analog-surround-21"
 
-pw-link sm57-carla:capture_FL alsa_output.usb-Topping_E2x2-00.analog-surround-21:playback_FL
-pw-link sm57-carla:capture_FR alsa_output.usb-Topping_E2x2-00.analog-surround-21:playback_FR
-pw-link soundboard_audio_sink:monitor_FL sm57-carla:input_FL
-pw-link soundboard_audio_sink:monitor_FR sm57-carla:input_FR
+LINKS=(
+    "alsa_input.usb-Topping_E2x2-00.analog-surround-21:capture_FL interface-in1_l:input_MONO"
+    "alsa_input.usb-Topping_E2x2-00.analog-surround-21:capture_FR interface-in2_r:input_MONO"
+    "sm57-carla:capture_FL $INTERFACE_NODE:playback_FL"
+    "sm57-carla:capture_FR $INTERFACE_NODE:playback_FR"
+    "soundboard_audio_sink:monitor_FL sm57-carla:input_FL"
+    "soundboard_audio_sink:monitor_FR sm57-carla:input_FR"
+    "chat_audio_sink:monitor_FL alsa_output.usb-Topping_E2x2-00.analog-surround-21:playback_FL"
+    "chat_audio_sink:monitor_FR alsa_output.usb-Topping_E2x2-00.analog-surround-21:playback_FR"
+    "game_audio_sink:monitor_FL alsa_output.usb-Topping_E2x2-00.analog-surround-21:playback_FL"
+    "game_audio_sink:monitor_FR alsa_output.usb-Topping_E2x2-00.analog-surround-21:playback_FR"
+)
+
+link () {
+    echo linking
+
+    for link in "${LINKS[@]}"; do
+        pw-link $link 2>/dev/null
+    done
+}
+
+echo "monitoring for $INTERFACE_NODE"
+
+while true; do
+    if pw-dump | grep -q "node\\.name\": \"$INTERFACE_NODE"; then
+        link
+
+        while pw-dump | grep -q "node\\.name\": \"$INTERFACE_NODE"; do
+            sleep 1
+        done
+
+        echo "device lost"
+    fi
+
+    sleep 1
+done
