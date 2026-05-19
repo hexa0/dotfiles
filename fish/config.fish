31e3c5a5ead30232a31aa62dfe6400a558d37fae
hexa0 <67880600+hexa0@users.noreply.github.com>
2026-05-18 21:35:34 -0400

    whatever tf i changed

--- fish/config.fish
+++ fish/config.fish
@@ -35,17 +35,21 @@ if test -d ~/.local/share/yabridge/
     export PATH="$PATH:$HOME/.local/share/yabridge"
 end
 
+# python
+
 if test -d ~/miniconda3/
     # why can't it do this by itself is beyond me
     source ~/miniconda3/etc/fish/conf.d/conda.fish
 end
 
 # literally nobody asked for these
+
 alias ffmpeg="ffmpeg -hide_banner"
 alias ffprobe="ffprobe -hide_banner"
 alias ffplay="ffplay -hide_banner"
 
-# termux specific stuff
+# termux stuff
+
 if test -f "$HOME/omen_ssh_ip.txt"
     set omen_ssh_ip $(cat "$HOME/omen_ssh_ip.txt")
 
@@ -54,17 +58,25 @@ if test -f "$HOME/omen_ssh_ip.txt"
     alias cl=clear
 end
 
+# spicetify
+
 if test -d ~/.spicetify/
 	fish_add_path ~/.spicetify
 end
+
+# dotnet
 
 if test -d ~/.dotnet/tools/
     fish_add_path ~/.dotnet/tools
 end
 
+# roblox wally
+
 if test -d ~/wally/
     fish_add_path ~/wally
 end
+
+# roblox zap
 
 if test -d ~/zap/
     fish_add_path ~/zap
@@ -74,9 +86,13 @@ if test -d ~/.path/
 	fish_add_path ~/.path
 end
 
+# roblox rokit
+
 if test -d ~/.rokit/bin/
 	fish_add_path  ~/.rokit/bin
 end
+
+# wii homebrew
 
 if test -d /opt/devkitpro
     set -gx DEVKITPRO /opt/devkitpro
@@ -84,6 +100,27 @@ if test -d /opt/devkitpro
     
     fish_add_path $DEVKITPRO/tools/bin
 end
+
+# ps2 homebrew
+
+if test -d /usr/local/ps2dev/
+	set -gx PS2DEV /usr/local/ps2dev
+	set -gx PS2SDK $PS2DEV/ps2sdk
+	set -gx GSKIT $PS2DEV/gsKit
+
+	fish_add_path $PS2DEV/bin
+	fish_add_path $PS2DEV/ee/bin
+	fish_add_path $PS2DEV/iop/bin
+	fish_add_path $PS2DEV/dvp/bin
+end
+
+# odin
+
+#if test -d ~/odin/
+#    fish_add_path ~/odin
+#end
+
+# why
 
 if test -d /usr/share/antigravity/
 	# not even sure if this will work but it's completely busted so whatever
